82c3ee84441e483deb4421d246d6ce4e8d97a7c1
theLemonday <nhathao090703@gmail.com>
2023-11-07 22:12:57 +0700

    update secret file

--- lib/Constant/Constant.h
+++ lib/Constant/Constant.h
@@ -3,27 +3,14 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
+#include <Secret.h>
 
-#define BLYNK_TEMPLATE_ID "TMPL6R2Gccxx7"
-#define BLYNK_TEMPLATE_NAME "Two Sensor One Actuator"
-#define BLYNK_AUTH_TOKEN "L3ezb9bvYGbRzzpMfnL5U9ewlOYIbep0"
-
-const char SSID[] = "Iot-Wifi";
-const char WIFI_PASSWORD[] = "11593195";
-// const char SSID[] = "Phòng máy 401";
-// const char WIFI_PASSWORD[] = "UET@#VNU";
-
-const char MQTT_USER[] = "croissantbeatpelican";
-const char MQTT_PASSWORD[] = "RF4c2D6n5TTd87";
-
-// const char HOST_IP[] = "192.168.0.101";
 const IPAddress HOST_IP(192, 168, 0, 101);
 const uint16_t HOST_PORT = 8080;
 
 const int BAUD_RATE = 115200;
 void setupBaudRate();
 
-// const unsigned long JSON_LENGTH = 100;
 const auto JSON_LENGTH = JSON_STRING_SIZE(3);
 
 #endif
